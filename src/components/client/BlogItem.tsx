536262ef6bdc1f15058c8b6b6e04b2fc9cd5953d
Tyrone Cloma <tyrone.cloma0219@gmail.com>
2023-07-09 18:42:34 -0400

    added blog page styling and basic animations

--- src/components/client/BlogItem.tsx
+++ src/components/client/BlogItem.tsx
@@ -4,5 +4,12 @@ interface Props {
 }
 
 export default function BlogItem({ title, slug }: Props) {
-	return <a href={`/blog/${slug}`}>{title}</a>
+	return (
+		<a
+			className="hover:font-bold w-full duration-200 border-b-2 py-4"
+			href={`/blog/${slug}`}
+		>
+			{title}
+		</a>
+	)
 }
