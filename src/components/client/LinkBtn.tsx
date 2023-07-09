a975de633070b384e6f41c8d7348f90ad995c8af
Tyrone Cloma <tyrone.cloma0219@gmail.com>
2023-07-09 19:08:31 -0400

    refactored LinkBtn component to be more modular

--- src/components/client/LinkBtn.tsx
+++ src/components/client/LinkBtn.tsx
@@ -1,16 +1,19 @@
 interface Props {
 	name: string
 	link: string
+	children: any
+	styles?: string
 }
 
-export default function LinkBtn({ name, link }: Props) {
+export default function LinkBtn({ name, link, children, styles }: Props) {
 	return (
 		<button
-			className=""
+			className={`flex h-full w-full items-center justify-center ${styles}`}
 			onClick={() => {
 				window.open(link)
 			}}
 		>
+			{children}
 			{name}
 		</button>
 	)
